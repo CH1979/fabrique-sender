fccce9fe10d529df7f2fb88e21b43598004944fa
Roman Polomoshnov <roman.polomoshnov@gmail.com>
2022-03-18 10:44:30 +0300

    Fix typo in settings

--- fabrique/settings.py
+++ fabrique/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'sender.apps.AppConfig',
+    'sender.apps.SenderConfig',
 ]
 
 MIDDLEWARE = [
